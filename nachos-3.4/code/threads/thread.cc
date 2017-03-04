c92ff607649cd86eaea959acbc5a4999369864b1
CrazyLegumes <deepwns@live.com>
2017-03-04 17:51:03 -0600

    Join has been completed

--- nachos-3.4/code/threads/thread.cc
+++ nachos-3.4/code/threads/thread.cc
@@ -259,7 +259,7 @@ Thread::Sleep ()
 {
     Thread *nextThread;
     
-    ASSERT(this == currentThread);
+    //ASSERT(this == currentThread);
     ASSERT(interrupt->getLevel() == IntOff);
     
     DEBUG('t', "Sleeping thread \"%s\"\n", getName());
